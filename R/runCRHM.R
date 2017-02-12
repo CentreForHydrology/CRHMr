82ba5f404aa258b749dced970cd2ea1a87062ad4
KevinShook <kevin.shook@usask.ca>
2017-02-12 12:35:13 -0600

    - no longer attempts to add quotes around files - allows multiple obs and par files

--- R/runCRHM.R
+++ R/runCRHM.R
@@ -1,6 +1,6 @@
 #' Runs a CRHM model
 #'
-#' @description Runs CRHM with a specified \code{.prj} file and (optionally) \code{.obs} file(s) and \code{.par} file(s). Under OSX and Linux, CRHM requires either 1) the program \code{Wine} to run \code{CRHM.exe} or 2) a native-compiled version. This function will use \code{Wine} if it is required.
+#' @description Runs CRHM with a specified \code{.prj} file and (optionally) \code{.obs} file(s) and \code{.par} file(s). Under OSX and Linux, CRHM requires either 1) the program \code{Wine} to run \code{CRHM.exe} or 2) a native-compiled version. This function will use \code{Wine} if it is required. Note that none of the file names or their paths can contain spaces.
 #' @param CRHMfile Required. CRHM executable file (usually \code{CRHM.exe}.
 #' @param prjFile Required. Name of \code{.prj} file. If the file does not contain any displayed observations or variables, or if any of the settings \code{Auto_Run}, \code{Auto_Exit} or \code{Log_All} are missing, then the function will terminate with an error message.
 #' @param obsFiles Optional. Name(s) of obs file(s).
@@ -13,71 +13,30 @@
 #' @seealso  \code{\link{automatePrj}}
 #' @examples
 #' \dontrun{
-#' result <- runCRHM('c:/CRHM/CRHM.exe', 'c:/BadLake/BadLake1975.prj', 
+#' result <- runCRHM('c:/CRHM/CRHM.exe', 'c:/BadLake/BadLake1975.prj',
 #' outfile='c:/BadLake/BadLake1975Output.txt')}
 #' @export
-runCRHM <- function(CRHMfile='', prjFile='', obsFiles='',  parFiles='', 
+runCRHM <- function(CRHMfile='', prjFile='', obsFiles='',  parFiles='',
                     outFile='', logfile='') {
   currentDir <- getwd()
   eol_val <- win.eol()
-  
-  # check parameters
-  if (CRHMfile==''){
-    cat('Missing CRHM file name\n')
-    return(FALSE)  
+
+  # check for spaces in file names
+  if(stringr::str_detect(CRHMfile ,' ')){
+    cat('Error: space present in CRHM executable file ath\n')
+    return(FALSE)
   }
-  
-  # check for executable file in specified path
-  CRHM_present <- file.exists(CRHMfile)
-  if (!CRHM_present){
-    cat("Error: can't find CRHM executable file\n")
-    return(FALSE)  
+
+  if(stringr::str_detect(prjFile ,' ')){
+    cat('Error: space present in .prj file path\n')
+    return(FALSE)
   }
-  
-  # check for project file
-  prj_present <- file.exists(prjFile)
-  if (!prj_present){
-    cat("Error: can't find CRHM project file\n")
-    return(FALSE)  
-  }
-  
-  # check contents of CRHM file to be sure that it's worth running
-  con <- file(prjFile, "r", blocking = FALSE, encoding="ISO_8859-2")
-  prj <- readLines(con)
-  close(con)
-  
-  display_variable <- readPrjLineText(prj, 'Display_Variable')
-  display_observation <- readPrjLineText(prj, 'Display_Observation')
-  
-  missing_display_variable <- sum(stringr::str_detect(display_variable, '#'))
-  missing_display_observation <- sum(stringr::str_detect(display_observation, '#'))
-  
-  if ((missing_display_variable > 0) & (missing_display_observation > 0)){
-    cat("Error: no variables output in .prj file\n")
-    return(FALSE)  
-  }
-  # check for Auto_Run, Log_All and Auto_Exit, if any are absent then quit
-  auto_run_present <- sum(stringr::str_detect(prj, 'Auto_Run'))
-  auto_exit_present <- sum(stringr::str_detect(prj, 'Auto_Exit'))  
-  log_all_present <- sum(stringr::str_detect(prj, 'Log_All'))
-  
-  if(auto_run_present <= 0){
-    cat("Error: missing Auto_Run command in .prj file\n")
-    return(FALSE)  
-  }
-  if(auto_exit_present <= 0){
-    cat("Error: missing Auto_Exit command in .prj file\n")
-    return(FALSE)  
-  }
-  if(log_all_present <= 0){
-    cat("Error: missing Log_All command in .prj file\n")
-    return(FALSE)  
-  }
-  
-  # figure out OS type and if Wine is being used  
+
+  # figure out OS type and if Wine is being used
   CRHM_file_name <- basename(CRHMfile)
   exe_present <- stringr::str_detect(CRHM_file_name, stringr::fixed('.exe', TRUE))
-  
+
+
   if (eol_val == '\n')
     OS_type <- 'Windows'
   else
@@ -89,52 +48,128 @@ runCRHM <- function(CRHMfile='', prjFile='', obsFiles='',  parFiles='',
   else
     # don't use wine - must be a native version present
     exe_prefix <- ''
-  
+
+  # check parameters
+  if (CRHMfile==''){
+    cat('Error: missing CRHM file name\n')
+    return(FALSE)
+  }
+
+
+  # check for executable file in specified path
+  CRHM_present <- file.exists(CRHMfile)
+  if (!CRHM_present){
+    cat("Error: can't find CRHM executable file\n")
+    return(FALSE)
+  }
+
+  # check for project file
+  prj_present <- file.exists(prjFile)
+
+
+  if (!prj_present){
+    cat("Error: can't find CRHM project file\n")
+    return(FALSE)
+  }
+
+
+  # check contents of CRHM file to be sure that it's worth running
+  con <- file(prjFile, "r", blocking = FALSE, encoding="ISO_8859-2")
+  prj <- readLines(con)
+  close(con)
+
+  display_variable <- readPrjLineText(prj, 'Display_Variable')
+  display_observation <- readPrjLineText(prj, 'Display_Observation')
+
+  missing_display_variable <- sum(stringr::str_detect(display_variable, '#'))
+  missing_display_observation <- sum(stringr::str_detect(display_observation, '#'))
+
+  if ((missing_display_variable > 0) & (missing_display_observation > 0)){
+    cat("Error: no variables output in .prj file\n")
+    return(FALSE)
+  }
+
+  # check for Auto_Run, Log_All and Auto_Exit, if any are absent then quit
+  auto_run_present <- sum(stringr::str_detect(prj, 'Auto_Run'))
+  auto_exit_present <- sum(stringr::str_detect(prj, 'Auto_Exit'))
+  log_all_present <- sum(stringr::str_detect(prj, 'Log_All'))
+
+  if(auto_run_present <= 0){
+    cat("Error: missing Auto_Run command in .prj file\n")
+    return(FALSE)
+  }
+  if(auto_exit_present <= 0){
+    cat("Error: missing Auto_Exit command in .prj file\n")
+    return(FALSE)
+  }
+  if(log_all_present <= 0){
+    cat("Error: missing Log_All command in .prj file\n")
+    return(FALSE)
+  }
+
+
   # get output file created by CRHM
   # find RUN_ID by reading prj file
 
   RUN_ID <- readPrjLine(prj, 'RUN_ID')
   CRHM_output_file <- paste('CRHM_output_', RUN_ID, '.txt', sep='')
-  
+
   if (exe_prefix == 'wine '){
-    CRHM_execution_string <- paste(exe_prefix, CRHMfile, ' ', '"', prjFile,'"', sep='')
+    prjFileBasename <- basename(prjFile)
+    CRHM_execution_string <- paste(exe_prefix,' ', CRHMfile, ' ', prjFileBasename, sep='')
     # copy obs and par files to location of prj file
     prj_dir <- dirname(prjFile)
     if (obsFiles != ''){
       numfiles <- length(obsFiles)
-      obsFiles <- basename(obsFiles)
+      obsFilesBasenames <- basename(obsFiles)
       for (i in 1:numfiles){
-        copy_command <- paste('cp ', obsFiles[i],'', prj_dir, sep='')
-        system(copy_command)
+        # check to see if obs file is already in prj directory
+        obs_dir <- dirname(obsFiles[i])
+        if (obs_dir != prj_dir){
+          copy_command <- paste('cp ', obsFiles[i],' ', prj_dir, sep='')
+          system(copy_command)
+        }
       }
-      obs <- stringr::str_c(obsFiles, collapse=' ')
-      CRHM_execution_string <- paste(CRHM_execution_string, obs, sep='')
+      obs <- stringr::str_c(obsFilesBasenames, collapse=' ')
+      if(stringr::str_detect(obs ,' ')){
+        cat('Error: space present in .obs file path\n')
+        return(FALSE)
+      }
+      CRHM_execution_string <- paste(CRHM_execution_string, obs, sep=' ')
     }
-  
+
     if (parFiles != ''){
       numfiles <- length(parFiles)
-      obsFiles <- basename(parFiles)
+      parFilesBasenames <- basename(parFiles)
       for (i in 1:numfiles){
-        copy_command <- paste('cp ', parFiles[i],'', prj_dir, sep = '')
-        system(copy_command)
+        obs_dir <- dirname(parFiles[i])
+        if (par_dir != prj_dir){
+          copy_command <- paste('cp ', parFiles[i],'', prj_dir, sep = '')
+          system(copy_command)
+        }
       }
-      par <- stringr::str_c(parFiles, collapse=' ')
-      CRHM_execution_string <- paste(CRHM_execution_string, par, sep = '')
-    }    
-    
+      par <- stringr::str_c(parFilesBasenames, collapse=' ')
+      if(stringr::str_detect(par ,' ')){
+        cat('Error: space present in parameter file path\n')
+        return(FALSE)
+      }
+      CRHM_execution_string <- paste(CRHM_execution_string, par, sep = ' ')
+    }
+
     # set working directory to be .prj directory
-    setwd(prj_dir)
-    
+    if (currentDir != prj_dir)
+      setwd(prj_dir)
+
     # now run CRHM
-    system(CRHM_execution_string)
-    
+    system2(CRHM_execution_string)
+
     # delete copied files
     if (obsFiles != ''){
       numfiles <- length(obsFiles)
       for (i in 1:numfiles)
         file.remove(obsFiles[i])
     }
-    
+
     if (parFiles != ''){
       numfiles <- length(parFiles)
       for (i in 1:numfiles)
@@ -145,28 +180,53 @@ runCRHM <- function(CRHMfile='', prjFile='', obsFiles='',  parFiles='',
     # running under Windows
     CRHM_execution_string <- CRHMfile
     CRHM_parameter_string <- prjFile
-    
-    if(obsFiles != '')
-      CRHM_parameter_string <- paste(CRHM_parameter_string, ' ', obsFiles, sep='')
-    
-    if(parFiles != '')
-      CRHM_parameter_string <- paste(CRHM_parameter_string, ' ', parFiles, sep='')
-    
+
+    if (length(obsFiles) > 1){
+      if(any(stringr::str_detect(obs ,' '))){
+        cat('Error: space present in .obs file path\n')
+        return(FALSE)
+      }
+      obs <- stringr::str_c(obsFiles, collapse=' ')
+
+      CRHM_parameter_string <- paste(CRHM_parameter_string, ' ', obs, sep='')
+    } else if(obsFiles != ''){
+      if(stringr::str_detect(obs ,' ')){
+        cat('Error: space present in .obs file path\n')
+        return(FALSE)
+      }
+      CRHM_parameter_string <- paste(CRHM_parameter_string, ' ', obs, sep='')
+    }
+
+    if (length(parFiles) > 1){
+      if(any(stringr::str_detect(par,' '))){
+        cat('Error: space present in parameter file path\n')
+        return(FALSE)
+      }
+      par <- stringr::str_c(parFiles, collapse=' ')
+      CRHM_parameter_string <- paste(CRHM_parameter_string, ' ', par, sep='')
+    } else if (parFiles != ''){
+      if(stringr::str_detect(par ,' ')){
+        cat('Error: space present in parameter file path\n')
+        return(FALSE)
+      }
+      CRHM_parameter_string <- paste(CRHM_parameter_string, ' ', par, sep='')
+    }
+
     # now call CRHM
-    system2(CRHM_execution_string, args=CRHM_parameter_string)
-    
+    system(CRHM_execution_string, args=CRHM_parameter_string)
+
     # create fake execution string for logging
     CRHM_execution_string <- paste(CRHM_execution_string, ' ', CRHM_parameter_string, sep='')
   }
-  
+
   # move/rename output
   file.rename(CRHM_output_file, outFile)
-  
+
   # reset working directory
   setwd(currentDir)
-  
+
   # log action
-  comment <- paste('runCRHM command:', CRHM_execution_string, sep='') 
+  comment <- paste('runCRHM command:', CRHM_execution_string, sep='')
   result <- logAction(comment, logfile)
   return(result)
 }
