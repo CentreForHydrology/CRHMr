0e6efc79a8d4ffc6ed60ebbfdc956cbf9084e305
Kevin Shook <kevin.shook@usask.ca>
2022-06-17 09:49:55 -0600

    added ability to add filter to obs files

--- R/writeObsFile.R
+++ R/writeObsFile.R
@@ -3,9 +3,20 @@
 #' @description Writes a CRHM dataframe to an obs file. It will not allow ea values - they must be converted to RH values first. The CRHM filter command to convert RH values to ea is added automatically to the header of the .obs file.
 #' @param obs Required. A \pkg{CRHMr} obs data frame.
 #' @param obsfile Required. Name for file to hold observations.
-#' @param comment Optional. A comment to be added to the header of the .obs file. If omitted, the header will consist of the name of the obs data frame followed by \option{created by CRHMr function writeObsFile}.
-#' @param quiet Optional. Suppresses display of messages, except for errors. If you are calling this function in an \R script, you will usually leave \code{quiet=TRUE} (i.e. the default). If you are working interactively, you will probably want to set \code{quiet=FALSE}.
-#' @param logfile Optional. Name of the file to be used for logging the action. Normally not used.
+#' @param comment Optional. A comment to be added to the header of the .obs file. If
+#' omitted, the header will consist of the name of the obs data frame
+#' followed by \option{created by CRHMr function writeObsFile}.
+#' @param obsfilter Optional. Default is \code{NULL} (not used). If specified, as a
+#' string scalar or vector, each value will be added to the file as an an
+#' observation filter. Commonly used to adjust 10m wind speed to 2m. Note
+#' that the observation filter for converting RH to ea is added autmatically,
+#' so you don't have to specify it.
+#' @param quiet Optional. Suppresses display of messages, except for errors. If you
+#' are calling this function in an \R script, you will usually leave \code{quiet=TRUE}
+#' (i.e. the default). If you are working interactively, you will probably want to
+#' set \code{quiet=FALSE}.
+#' @param logfile Optional. Name of the file to be used for logging the action.
+#' Normally not used.
 #' @author Kevin Shook
 #' @note If there are missing values in the obs dataframe, the last line of the header will contain the line \option{$$ Missing}.
 #' @return If successful, returns \code{TRUE}. If unsuccessful, returns \code{FALSE}.
@@ -15,12 +26,12 @@
 #' result <- writeObsFile(BadLake7376, 'BadLake7376.obs')}
 #' @export
 
-writeObsFile <- function(obs, obsfile='',  comment='', quiet=TRUE, logfile=''){
+writeObsFile <- function(obs, obsfile="", obsfilter=NULL, comment="", quiet=TRUE, logfile=""){
   eol.val <- win.eol()
 
   # check parameters
   rh.filter <- '$ea ea(t, rh)'
-  if (obsfile == ''){
+  if (obsfile == "") {
     cat('Missing .obs file name\n')
     return(FALSE)
   }
@@ -29,8 +40,8 @@ writeObsFile <- function(obs, obsfile='',  comment='', quiet=TRUE, logfile=''){
 
   obs.names <- names(obs)[-1]
   obs.names.uppercase <- toupper(obs.names)
-  ea.loc <-grep("EA.", obs.names.uppercase, fixed=TRUE)
-  rh.loc <-grep("RH", obs.names.uppercase, fixed=TRUE)
+  ea.loc <- grep("EA.", obs.names.uppercase, fixed = TRUE)
+  rh.loc <- grep("RH", obs.names.uppercase, fixed = TRUE)
 
   if (length(ea.loc) > 0)
     ea.present <- TRUE
@@ -42,7 +53,7 @@ writeObsFile <- function(obs, obsfile='',  comment='', quiet=TRUE, logfile=''){
   else
     rh.present <- FALSE
 
-  if (ea.present & rh.present){
+  if (ea.present & rh.present) {
     cat("Error: you can't have both ea and RH values in an obs file\n")
     return(FALSE)
   }
@@ -53,8 +64,8 @@ writeObsFile <- function(obs, obsfile='',  comment='', quiet=TRUE, logfile=''){
 
   # assemble header
   if (comment == '')
-    comment <- paste(obsName,' created by CRHMr function writeObsFile', sep='')
-  cat(comment, eol.val, sep='', file=obsfile, append=FALSE)
+    comment <- paste(obsName,' created by CRHMr function writeObsFile', sep = '')
+  cat(comment, eol.val, sep = '', file = obsfile, append = FALSE)
 
   # find unique variable names
   obs.strings <- stringr::str_split_fixed(obs.names, stringr::fixed('.'), 3)
@@ -87,7 +98,14 @@ writeObsFile <- function(obs, obsfile='',  comment='', quiet=TRUE, logfile=''){
   }
 
   if (rh.present)
-    cat(rh.filter, eol.val, sep='', file=obsfile, append=TRUE)
+    cat(rh.filter, eol.val, sep = "", file = obsfile, append = TRUE)
+
+  if (!is.null(obsfilter)) {
+    for (i in 1:length(obsfilter)) {
+      cat(obsfilter[i], eol.val, sep = "", file = obsfile, append = TRUE)
+    }
+  }
+
 
   # check to see if missing values, and indicate in obsfile
   obs.clean <- na.omit(obs)
