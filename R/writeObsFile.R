31caa4f8d544527b208521b229c3108e739fffe2
Kevin Shook <kevin.shook@usask.ca>
2022-06-24 13:16:22 -0600

    updated writeObsFile headers

--- R/writeObsFile.R
+++ R/writeObsFile.R
@@ -1,6 +1,8 @@
 #' Writes a dataframe to a .obs file
 #'
-#' @description Writes a CRHM dataframe to an obs file. It will not allow ea values - they must be converted to RH values first. The CRHM filter command to convert RH values to ea is added automatically to the header of the .obs file.
+#' @description Writes a CRHM dataframe to an obs file. It will not allow ea values - they must be
+#' converted to RH values first. The CRHM filter command to convert RH values to ea is added
+#' automatically to the header of the .obs file.
 #' @param obs Required. A \pkg{CRHMr} obs data frame.
 #' @param obsfile Required. Name for file to hold observations.
 #' @param comment Optional. A comment to be added to the header of the .obs file. If
@@ -18,7 +20,8 @@
 #' @param logfile Optional. Name of the file to be used for logging the action.
 #' Normally not used.
 #' @author Kevin Shook
-#' @note If there are missing values in the obs dataframe, the last line of the header will contain the line \option{$$ Missing}.
+#' @note If there are missing values in the obs dataframe, the last line of the header will contain
+#' the line \option{$$ Missing}.
 #' @return If successful, returns \code{TRUE}. If unsuccessful, returns \code{FALSE}.
 #' @seealso  \code{\link{trimObs}} \code{\link{changeEatoRH}}
 #' @examples \dontrun{
@@ -77,24 +80,24 @@ writeObsFile <- function(obs, obsfile="", obsfilter=NULL, comment="", quiet=TRUE
   var.units[var.names == 'u'] <- '(m/s)'
   var.units[var.names == 'u10'] <- '(m/s)'
   var.units[var.names == 'u2'] <- '(m/s)'
-  var.units[var.names == 'p'] <- '(mm)'
+  var.units[var.names == 'p'] <- '(mm/int)'
   var.units[var.names == 'ppt'] <- '(mm)'
   var.units[var.names == 'ea'] <- '(kPa)'
-  var.units[tolower(var.names) == 'qsi'] <- '(W/m2)'
-  var.units[tolower(var.names) == 'qso'] <- '(W/m2)'
-  var.units[tolower(var.names) == 'qsn'] <- '(W/m2)'
-  var.units[tolower(var.names) == 'qli'] <- '(W/m2)'
-  var.units[tolower(var.names) == 'qlo'] <- '(W/m2)'
-  var.units[tolower(var.names) == 'qln'] <- '(W/m2)'
-  var.units[tolower(var.names) == 'qn'] <- '(W/m2)'
+  var.units[tolower(var.names) == 'qsi'] <- '(W/m^2)'
+  var.units[tolower(var.names) == 'qso'] <- '(W/m^2)'
+  var.units[tolower(var.names) == 'qsn'] <- '(W/m^2)'
+  var.units[tolower(var.names) == 'qli'] <- '(W/m^2)'
+  var.units[tolower(var.names) == 'qlo'] <- '(W/m^2)'
+  var.units[tolower(var.names) == 'qln'] <- '(W/m^2)'
+  var.units[tolower(var.names) == 'qn'] <- '(W/m^2)'
 
   # find number of each type of variable
   # can't use a table as it sorts alphabetically
   num.vars <- length(var.names)
-  for (i in 1:num.vars){
+  for (i in 1:num.vars) {
     var.count <- sum(obs.strings[,1] == var.names[i])
-    cat(var.names[i],'\t', var.count, ' ', var.units[i], eol.val, sep='',
-        file=obsfile, append=TRUE)
+    cat(var.names[i], '\t', var.count, ' ', var.units[i], eol.val, sep = '',
+        file = obsfile, append = TRUE)
   }
 
   if (rh.present)
@@ -110,11 +113,12 @@ writeObsFile <- function(obs, obsfile="", obsfilter=NULL, comment="", quiet=TRUE
   # check to see if missing values, and indicate in obsfile
   obs.clean <- na.omit(obs)
   if (nrow(obs.clean) != nrow(obs))
-    cat('$$ Missing',eol.val, sep='', file=obsfile, append=TRUE)
+    cat('$$ Missing',eol.val, sep = '', file = obsfile, append = TRUE)
 
   # add names to columns
-  colnames <- stringr::str_c(obs.names, collapse='\t')
-  cat('################','\t',colnames, eol.val, sep='', file=obsfile, append=TRUE)
+  colnames <- stringr::str_c(obs.names, collapse = '\t')
+  cat('################','\t',colnames, eol.val, sep = '',
+      file = obsfile, append = TRUE)
 
 
   # output info to screen and write to log file
@@ -124,14 +128,14 @@ writeObsFile <- function(obs, obsfile="", obsfilter=NULL, comment="", quiet=TRUE
 
   # write values to file
 
-  obs[,1] <- format(obs[,1], format='%Y %m %d %H %M')
-  write.table(obs, file=obsfile, sep='\t', eol=eol.val,
-              row.names=FALSE, quote=FALSE,
-              col.names=FALSE, append=TRUE)
+  obs[,1] <- format(obs[,1], format = '%Y %m %d %H %M')
+  write.table(obs, file = obsfile, sep = '\t', eol = eol.val,
+              row.names = FALSE, quote = FALSE,
+              col.names = FALSE, append = TRUE)
 
 
   comment <- paste('writeObsFile dataframe:', obsName,
-                   ' .obs file: ', obsfile, sep='')
+                   ' .obs file: ', obsfile, sep = '')
   result <- logAction(comment, logfile)
 
   return(TRUE)
